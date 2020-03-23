4349ac7e4734e4867955932717c4995b21d26078
Brian Tribondeau <brian.tribondeau@cfm.fr>
2020-03-23 14:45:15 +0100

    Fix PEP8

--- jupytab-server/tests/test_config.py
+++ jupytab-server/tests/test_config.py
@@ -19,7 +19,7 @@ def config(config_file):
 def test_ssl():
     with pytest.raises(FileNotFoundError):
         config_ssl(config('config/ssl_bad_file.ini'))
-    assert config_ssl(config('config/ssl_disabled.ini')) == None
-    assert config_ssl(config('config/ssl_none.ini')) == None
+    assert config_ssl(config('config/ssl_disabled.ini')) is None
+    assert config_ssl(config('config/ssl_none.ini')) is None
     assert config_ssl(config('config/ssl_ok.ini')) == {'certfile': 'config/example.cert',
                                                        'keyfile': 'config/example.key'}
