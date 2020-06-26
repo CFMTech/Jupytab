8fd7ff4ceabf6a9b1679928b31a85fc0a941d95c
Jean-Sebastien Dieu <jean-sebastien.dieu@cfm.fr>
2020-06-26 16:44:07 +0200

    #38 - Rework package mechanism to include version information

--- jupytab-server/setup.py
+++ jupytab-server/setup.py
@@ -1,11 +1,47 @@
 # Copyright (c) Capital Fund Management
 # Distributed under the terms of the MIT License.
 
-import os
 from setuptools import setup, find_packages
 
-with open(os.path.join(os.path.dirname(__file__), '..', 'VERSION')) as version_file:
-    VERSION = version_file.read().strip()
+import distutils.cmd
+import pathlib
+
+
+class MakeVersionCommand(distutils.cmd.Command):
+    """Prepare the version of jupytab-server"""
+
+    description = "add extra file to make the version"
+    user_options = [] 
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+       version_file = pathlib.Path(__file__).parent.resolve()
+       version_file = version_file.parent / 'VERSION'
+       version = version_file.resolve().read_text().strip()
+       version_py_file = pathlib.Path(__file__).parent / 'jupytab_server' /  '__version__.py'
+       with version_py_file.open('w') as stream:
+           stream.write(f'__version__="{version}"')
+
+
+def read_version():
+    version = pathlib.Path(__file__).parent.resolve()
+    version_master = version.parent  / 'VERSION'
+    version_master.resolve()
+    version_4_release = version / 'jupytab_server' / '__version__.py'
+    if version_4_release.exists():
+        v = version_4_release.read_text().strip().split('=')[1]
+        # Let's remove quotes
+        return v[1:len(v) - 1]
+    elif version_master.exists():
+        return version_master.read_text().strip()
+    else:
+        raise FileNotFound('no version file can be found')
+
 
 README = """
 
@@ -31,9 +67,9 @@ The full documentation is available on the project's home page.
 """
 
 # This call to setup() does all the work
-setup(
+setup(cmdclass=dict(make_version=MakeVersionCommand),
     name="jupytab-server",
-    version=VERSION,
+    version=read_version(),
     description="Connect Tableau to your Jupyter Notebook",
     long_description_content_type="text/markdown",
     long_description=README,
--- jupytab/setup.py
+++ jupytab/setup.py
@@ -1,11 +1,47 @@
 # Copyright (c) Capital Fund Management
 # Distributed under the terms of the MIT License.
 
-import os
 from setuptools import setup, find_packages
 
-with open(os.path.join(os.path.dirname(__file__), '..', 'VERSION')) as version_file:
-    VERSION = version_file.read().strip()
+import distutils.cmd
+import pathlib
+
+
+class MakeVersionCommand(distutils.cmd.Command):
+    """Prepare the version of jupytab"""
+
+    description = "add extra file to make the version"
+    user_options = [] 
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+       version_file = pathlib.Path(__file__).parent.resolve()
+       version_file = version_file.parent / 'VERSION'
+       version = version_file.resolve().read_text().strip()
+       version_py_file = pathlib.Path(__file__).parent / 'jupytab' /  '__version__.py'
+       with version_py_file.open('w') as stream:
+           stream.write(f'__version__="{version}"')
+
+
+def read_version():
+    version = pathlib.Path(__file__).parent.resolve()
+    version_master = version.parent  / 'VERSION'
+    version_master.resolve()
+    version_4_release = version / 'jupytab' / '__version__.py'
+    if version_4_release.exists():
+        v = version_4_release.read_text().strip().split('=')[1]
+        # Let's remove quotes
+        return v[1:len(v) - 1]
+    elif version_master.exists():
+        return version_master.read_text().strip()
+    else:
+        raise FileNotFound('no version file can be found')
+
 
 README = """
 
@@ -22,9 +58,9 @@ The full documentation is available on the project's home page.
 """
 
 # This call to setup() does all the work
-setup(
+setup(cmdclass=dict(make_version=MakeVersionCommand),
     name="jupytab",
-    version=VERSION,
+    version=read_version(),
     description="Jupytab package to be used in notebooks",
     long_description_content_type="text/markdown",
     long_description=README,
